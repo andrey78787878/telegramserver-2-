5e796953ea663f63bb83565fcf8a0379dc5ba5b1
andrey78787878 <andrey.tkach78@gmail.com>
2025-08-19 21:54:22 +0500

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,16 @@
+[tool.poetry]
+name = "telegram-bot"
+version = "0.1.0"
+description = "Telegram Audit Bot"
+authors = ["Your Name <your@email.com>"]
+
+[tool.poetry.dependencies]
+python = "^3.11"
+python-telegram-bot = "^21.9"
+requests = "^2.31.0"
+
+[tool.poetry.group.dev.dependencies]
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
